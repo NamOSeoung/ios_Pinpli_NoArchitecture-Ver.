b34b81578e7cfed24cef8eee689f3d378ad3218b
NamOSeoung <developernam92@gmail.com>
2020-09-15 17:38:04 +0900

    Initial Commit

---
+++ Review/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Review
+//
+//  Created by 남오승 on 2020/09/15.
+//  Copyright © 2020 nam_os. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
